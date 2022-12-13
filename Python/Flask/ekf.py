b7853d84ad8c39c67ad242d1c62928e7d0d78e82
Jose <jocamaba1989@gmail.com>
2022-12-13 12:35:48 -0600

    handmade ekf

--- Python/Flask/ekf.py
+++ Python/Flask/ekf.py
@@ -1,62 +1,29 @@
 import numpy as np
-import tkinter
-from filterpy.kalman import ExtendedKalmanFilter
-from filterpy.common import Q_discrete_white_noise
-import matplotlib.pyplot as plt
-import time
 
-class EKF(ExtendedKalmanFilter):
-    def do_something_amazing(self):
-        plt.plot(self.x, c="blue")
-        plt.show()
-        time.sleep(0.1)
-   
-    def calculate_jacobian(self,x):
-        #print(np.array(x * np.random.rand()))
-        #return np.array(x * np.random.rand()).T
-        #J = np.zeros((2,1))
-        J = np.eye(2)
-        #J[0,1] = self.dt
-        return J #np.random.rand()
+px = 0
+py = 0
+vx = 0.1
+vy = 0
 
-    def Hx(self,x):
-        #print ("PM", self.H.dot(z))
-        Hx = np.eye(2)
-        #print("HX" ,self.x - np.dot(self.K, self.y))
-        #Hx[1] = 1
-        return Hx#Hx.dot(x)
+dt = 0.1
 
-    def __init__(self):
-        #state number of variables, measurements
-        super().__init__(dim_x=2, dim_z=2, dim_u=1)
-        self.i = 0
-        self.dt = 0.1
-        self.data = []
+#State
+x = np.asarray(([px, py,vx, vy]))
+P = np.eye(4)
 
-        self.B = np.eye(2)
+#State Transition
+F = np.eye(4)
+F[0,2] = F[1,3]  = dt
 
-        self.x = np.array(([2],[2])   # initial state (location and velocity)
+#Noise
+G = np.zeros((4,2))
+G[0,0] = G[1,1] = np.power(dt,2)/2
+G[2,0] = G[3,1] = dt
 
-        self.F = np.array([[1.,self.dt],
-                        [0.,1.]])    # state transition matrix
+std_dev = np.eye(2)
+Q = G.dot(std_dev).dot(G.T) #assumed accelerations
 
-        #self.H = np.array([[1.,0.]])    # Measurement function
-        self.P *= 1.                 # covariance matrix
-        self.R = 1                      # state uncertainty
-        self.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.1) # process uncertainty
-
-    def get_measurement(self):
-        self.i += 1
-        return 1,1#[self.i, self.i]#np.random.rand()
-
-
-if __name__ == '__main__':
-    my_filter = EKF()
-    while True:
-        my_filter.predict()
-        print("AF", my_filter.x)
-        my_filter.update(None, my_filter.calculate_jacobian, my_filter.Hx)
-        print("BF", my_filter.x)
-        # do something with the output
-        my_filter.do_something_amazing()
-
+#prediction
+x_prime = np.dot(F,x)
+print(x_prime)
+P_prime = F.dot(P).dot(F.T) + Q
