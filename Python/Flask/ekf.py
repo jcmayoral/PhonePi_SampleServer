17666869d8fb2b4c198aebc48836d53367b13c0a
Jose <jocamaba1989@gmail.com>
2022-12-13 13:12:12 -0600

    custom

--- Python/Flask/ekf.py
+++ Python/Flask/ekf.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+#EXAMPLE TO DETECT PERSON
+
 px = 0
 py = 0
 vx = 0.1
@@ -8,7 +10,8 @@ vy = 0
 dt = 0.1
 
 #State
-x = np.asarray(([px, py,vx, vy]))
+x = np.asarray(([px, py,vx, vy])).reshape(4,1)
+print(x.shape)
 P = np.eye(4)
 
 #State Transition
@@ -24,6 +27,40 @@ std_dev = np.eye(2)
 Q = G.dot(std_dev).dot(G.T) #assumed accelerations
 
 #prediction
-x_prime = np.dot(F,x)
-print(x_prime)
+x_prime = np.dot(F,x).reshape(4,1)
+print(x_prime.shape)
 P_prime = F.dot(P).dot(F.T) + Q
+
+
+#Measurement Update
+#LIDAR POSITION
+#RADAR SPEED
+#X Y Measurement
+z = np.ones(2).reshape(2,1)
+
+#Measurement Matrix for KF
+H = np.zeros((2,4))
+H[0,0] = H[1,1] = 1
+
+#FOR EKF
+def get_HJacob(x_prime):
+    #TO SIMPLIFY
+    H = np.zeros((2,4))
+    H[0,0] = H[1,1] = 1
+    return H
+
+H = get_HJacob(x_prime)
+
+#Sensor Noise
+R = np.zeros((2,2))
+R[0,0] = R[1,1] = 0.01
+
+
+y = z - H.dot(x_prime)
+S = H.dot(P_prime).dot(H.T) + R
+K = P_prime.dot(H.T).dot(np.linalg.inv(S))
+x = x_prime + K.dot(y)
+I = np.eye(4)
+P = (I - K.dot(H)).dot(P_prime)
+
+print(x.shape)
